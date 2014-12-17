b7595fbe781193c3745d15c4081e65e5180ff830
ackl <contact@ackl.me>
2014-12-17 21:48:19 +0000

    Recover new album view from compiled js file
    * (forgot to track the files)

---
+++ static/js/views/betteralbumview.js
@@ -0,0 +1,34 @@
+var $ = require('jquery');
+var Backbone = require('backbone');
+var _ = require('underscore');
+var template = require('../../templates/albumsonglisttemplate.html');
+var AlbumSongListView = require('./albumsonglistview');
+Backbone.$ = $;
+
+var AlbumSongItemView = require('./albumsongitemview');
+
+module.exports = Backbone.View.extend({
+	initialize: function() {
+		console.log(this.model.art);
+	},
+	events: {
+		"click #shrink-album-detail": "shrink"
+	},
+	shrink: function() {
+		this.$('.album-detail-content').empty();
+		this.$el.animate({width:'toggle'},550);
+		this.$('.album-detail-content').animate({width:'toggle'},550);
+	},
+	render: function() {
+		var theSongs = new AlbumSongListView({collection: this.model.items});
+		this.$el.html(template({
+			name: this.model.album,
+			artist: this.model.albumartist,
+			url: '/album/'+this.model.id+'/art'
+		}));
+		this.$('.album-detail-content').append(theSongs.render().el).append(' <button class="btn shrink-btn" id="shrink-album-detail">shrink</button>');
+		return this;
+	}
+});
+
+
